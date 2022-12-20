edf3c6b90ca68f617c38a0772199b6cb1ca9e857
Achinit99 <achinit720@gmail.com>
2022-12-20 19:05:56 +0530

    php numbers

--- 03_numbers.php
+++ 03_numbers.php
@@ -1,16 +1,38 @@
 <?php
 
 // Declaring numbers
+    $a = 5;
+    $b = 4;
+    $c = 1.2;
 
 // Arithmetic operations
+    echo ($a + $b) * $c.'<br>';
+    echo $a - $b . '<br>';
+    echo $a * $b . '<br>';
+    echo $a / $b . '<br>';
+    echo $a % $b . '<br><br>';
 
 // Assignment with math operators
+    //$a += $b; echo $a.'<br>';
+    //$a -= $b; echo $a.'<br>';
+    //$a *= $b; echo $a.'<br>';
+    //$a /= $b; echo $a.'<br>';
+    //$a %= $b; echo $a.'<br>';
 
 // Increment operator
+    echo  $a++ . '<br>';
+    echo ++$a . '<br><br>';
 
 // Decrement operator
+    echo $a-- . '<br>';
+    echo --$a . '<br><br>';
 
 // Number checking functions
+    is_float(1.25); // ture
+    is_double(1.25); //ture
+    is_int(5); // ture
+    is_numeric("3.45"); // ture
+    is_numeric("3g.45"); // false
 
 // Conversion
 
