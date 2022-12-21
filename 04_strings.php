8ccd3004a7d18a0bd7a1121701e69818c11707be
Achinit99 <achinit720@gmail.com>
2022-12-21 09:10:48 +0530

    String functions

--- 04_strings.php
+++ 04_strings.php
@@ -8,9 +8,27 @@ echo $string.'<br>';
 echo $string2.'<br><br>';
 
 // String concatenation
-echo 'Hello'.'World'.' and PHP'.'<br>';
+echo 'Hello'.'World'.' and PHP'.'<br><br>';
 
 // String functions
+$string = "     Hello World     ";
+
+echo "1 - ".strlen($string).'<br>';
+echo "2 - ".trim($string).'<br>';
+echo "3 - ".ltrim($string).'<br>';
+echo "4 - ".rtrim($string).'<br>';
+echo "5 - ".str_word_count($string).'<br>';
+echo "6 - ".strrev($string).'<br>';
+echo "7 - ".strtoupper($string).'<br>';
+echo "8 - ".strtolower($string).'<br>';
+echo "9 - ".ucfirst('hello').'<br>';
+echo "10 - ".lcfirst('HELLO').'<br>';
+echo "11- ".ucwords('hello world and php').'<br>';
+echo "12 - ".strpos($string, 'world').'<br>';
+echo "13 - ".stripos($string, 'world').'<br>';
+echo "14 - ".substr($string, 8).'<br>';
+echo "15 - ".str_replace('world', 'PHP', $string).'<br>';
+echo "16 - ".str_ireplace('world', 'PHP', $string).'<br>';
 
 // Multiline text and line breaks
 
