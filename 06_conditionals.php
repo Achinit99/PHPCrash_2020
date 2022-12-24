7f643a42545c0f2ef98db798a989eece26a3dfe5
Achinit99 <achinit720@gmail.com>
2022-12-24 22:24:25 +0530

    conditionals

--- 06_conditionals.php
+++ 06_conditionals.php
@@ -4,12 +4,27 @@ $age = 20;
 $salary = 300000;
 
 // Sample if
+// if ($age === 20) {
+//   echo "1";
+// }
 
 // Without circle braces
+// if ($age === 20) echo "1";
+
 
 // Sample if-else
+// if ($age > 20) {
+//   echo "1";
+// } else {
+//   echo "2";
+// }
 
 // Difference between == and ===
+$age == 20; // true
+$age == '20'; // true
+
+$age === 20; // true
+$age === '20'; // false
 
 // if AND
 
