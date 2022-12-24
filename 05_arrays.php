5cdcc1a3965fc06423500951f00af2c4c82b32b8
Achinit99 <achinit720@gmail.com>
2022-12-24 16:36:24 +0530

    Set element by key

--- 05_arrays.php
+++ 05_arrays.php
@@ -1,93 +1,93 @@
 <?php
 
 // Create array
-$fruits = ["Banana", "Apple", "Orange"];
+// $fruits = ["Banana", "Apple", "Orange"];
 
-// Print the whole array
-echo '<pre>';
-var_dump($fruits);
-echo '</pre>';
+// // Print the whole array
+// echo '<pre>';
+// var_dump($fruits);
+// echo '</pre>';
 
 
-// Get element by index
-echo $fruits[2] . '<br>';
+// // Get element by index
+// echo $fruits[2] . '<br>';
 
-// Set element by index
-$fruits[0] = 'Peach';
-echo '<pre>';
-var_dump($fruits);
-echo '</pre>';
+// // Set element by index
+// $fruits[0] = 'Peach';
+// echo '<pre>';
+// var_dump($fruits);
+// echo '</pre>';
 
-// Check if array has element at index 2
-isset($fruits[3]); // false
+// // Check if array has element at index 2
+// isset($fruits[3]); // false
 
-// Append element
-$fruits[] = 'Banana';
-echo '<pre>';
-var_dump($fruits);
-echo '</pre>';
+// // Append element
+// $fruits[] = 'Banana';
+// echo '<pre>';
+// var_dump($fruits);
+// echo '</pre>';
 
-// Print the length of the array
-echo count($fruits).'<br>';
+// // Print the length of the array
+// echo count($fruits).'<br>';
 
-// Add element at the end of the array
-array_push($fruits, 'foo');
-echo '<pre>';
-var_dump($fruits);
-echo'</pre>';
+// // Add element at the end of the array
+// array_push($fruits, 'foo');
+// echo '<pre>';
+// var_dump($fruits);
+// echo'</pre>';
 
-// Remove element from the end of the array
-echo array_pop($fruits);
-echo '<pre>';
-var_dump($fruits);
-echo'</pre>';
+// // Remove element from the end of the array
+// echo array_pop($fruits);
+// echo '<pre>';
+// var_dump($fruits);
+// echo'</pre>';
 
-// Add element at the beginning of the array
-array_unshift($fruits, 'bar');
-echo '<pre>';
-var_dump($fruits);
-echo'</pre>';
+// // Add element at the beginning of the array
+// array_unshift($fruits, 'bar');
+// echo '<pre>';
+// var_dump($fruits);
+// echo'</pre>';
 
-// Remove element from the beginning of the array
-echo array_shift($fruits);
-echo '<pre>';
-var_dump($fruits);
-echo'</pre>';
+// // Remove element from the beginning of the array
+// echo array_shift($fruits);
+// echo '<pre>';
+// var_dump($fruits);
+// echo'</pre>';
 
-// Split the string into an array
-$string = "Banana, Apple, Peach";
-echo '<pre>'; 
-var_dump(explode(",", $string));
-echo'</pre>';
+// // Split the string into an array
+// $string = "Banana, Apple, Peach";
+// echo '<pre>'; 
+// var_dump(explode(",", $string));
+// echo'</pre>';
 
-// Combine array elements into string
-echo implode("&", $fruits);
+// // Combine array elements into string
+// echo implode("&", $fruits);
 
-// Check if element exist in the array
-echo '<pre>'; 
-var_dump(in_array('Apple', $fruits));
-echo'</pre>';
+// // Check if element exist in the array
+// echo '<pre>'; 
+// var_dump(in_array('Apple', $fruits));
+// echo'</pre>';
 
-// Search element index in the array
-echo '<pre>'; 
-var_dump(array_search('Apple', $fruits));
-echo'</pre>';
+// // Search element index in the array
+// echo '<pre>'; 
+// var_dump(array_search('Apple', $fruits));
+// echo'</pre>';
 
-// Merge two arrays
-$vegetables =["Potato", "Cucumber"];
-echo '<pre>'; 
-var_dump(array_merge($fruits, $vegetables));
-var_dump([...$fruits, ...$vegetables]);
-echo'</pre>';
+// // Merge two arrays
+// $vegetables =["Potato", "Cucumber"];
+// echo '<pre>'; 
+// var_dump(array_merge($fruits, $vegetables));
+// var_dump([...$fruits, ...$vegetables]);
+// echo'</pre>';
 
-// Sorting of array (Reverse order also)
-echo '<pre>'; 
-var_dump($fruits);
-echo'</pre>';
-rsort($fruits);
-echo '<pre>'; 
-var_dump($fruits);
-echo'</pre>';
+// // Sorting of array (Reverse order also)
+// echo '<pre>'; 
+// var_dump($fruits);
+// echo'</pre>';
+// rsort($fruits);
+// echo '<pre>'; 
+// var_dump($fruits);
+// echo'</pre>';
 
 // https://www.php.net/manual/en/ref.array.php
 
@@ -96,10 +96,23 @@ echo'</pre>';
 // ============================================
 
 // Create an associative array
-
+$person = [
+  'name' => 'Achini',
+  'surname' => 'Thathsarani',
+  'age' => 23,
+  'hobbies' => ['Teniss', 'Video Game']
+];
+ echo '<pre>';
+ print_r($person);
+ echo '</pre>';
 // Get element by key
+echo $person['name'].'<br>';
 
 // Set element by key
+$person['channel'] = 'SL Point';
+echo '<pre>';
+var_dump($person);
+echo '</pre>';
 
 // Null coalescing assignment operator. Since PHP 7.4
 
