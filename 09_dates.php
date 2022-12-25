2fcc44c909cc968f965ff4d00103baf1f530d97a
Achinit99 <achinit720@gmail.com>
2022-12-25 22:48:14 +0530

    php date

--- 09_dates.php
+++ 09_dates.php
@@ -1,10 +1,13 @@
 <?php
 
 // Print current date
+echo date('Y-m-d H:i:s').'<br>';
 
 // Print yesterday
+echo date('Y-m-d H:i:s', time() - 60 * 60 * 24).'<br>';
 
 // Different format: https://www.php.net/manual/en/function.date.php
+echo date('F j Y, H:i:s');
 
 // Print current timestamp
 
