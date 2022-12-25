878e005fcd1cc6f90a697557779fe360594343a5
Achinit99 <achinit720@gmail.com>
2022-12-25 23:59:21 +0530

    parsedDate

--- 09_dates.php
+++ 09_dates.php
@@ -7,10 +7,15 @@ echo date('Y-m-d H:i:s').'<br>';
 echo date('Y-m-d H:i:s', time() - 60 * 60 * 24).'<br>';
 
 // Different format: https://www.php.net/manual/en/function.date.php
-echo date('F j Y, H:i:s');
+echo date('F j Y, H:i:s').'<br>';
 
 // Print current timestamp
+echo time().'<br>';
 
-// Parse date: https://www.php.net/manual/en/function.date-parse.php
+// parsedDate: https://www.php.net/manual/en/function.date-parse.php
+$parsedDate = date_parse('2022-12-25 11:56:00');
+echo '<pre>';
+var_dump($parsedDate);
+echo '</pre>';
 
 // Parse date from format: https://www.php.net/manual/en/function.date-parse-from-format.php
