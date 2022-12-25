67836bba76a4c15fb15148a73164385578d559c1
Achinit99 <achinit720@gmail.com>
2022-12-25 21:08:53 +0530

    loop

--- 07_loops.php
+++ 07_loops.php
@@ -1,13 +1,48 @@
 <?php
 
 // while
+/* while (true) {
 
+}
+ */
 // Loop with $counter
+ /*$counter = 0;
+ while ($counter <= 10) {
+  echo $counter.'<br>';
+  // if ($counter === 5) {
+  //   break;
+  // }
+  $counter++;
+}  */
 
 // do - while
-
+/* do {
+  echo $counter.'<br>';
+  $counter++;
+} while ($counter < 0);
+ */
 // for
+/* for ($i = 0; $i < 10; $i++) {
+  echo $i.'<br>';
+}
 
 // foreach
+$fruits = ["Banana", "Apple", "Orange"];
+foreach ($fruits as $i => $newfruits) {
+  echo $i. ' '.$newfruits. '<br>';
+} */
 
 // Iterate Over associative array.
+/* $person = [
+  'name' => 'Achini',
+  'surname' => 'Thathsarani',
+  'age' => 23,
+  'hobbies' => ['Teniss', 'Video Game']
+]; 
+foreach ($person as $key => $value) {
+  if (is_array($value)) {
+  echo $key.' '.implode(", ", $value).'<br>';
+} else {
+  echo $key.' '. $value.'<br>';
+}
+} */
