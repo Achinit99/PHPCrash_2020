06e69cca82e641917550d399a7a16b3ecc6b87f8
Achinit99 <achinit720@gmail.com>
2022-12-25 21:41:29 +0530

    Create function to sum all numbers using ...$nums

--- 08_functions.php
+++ 08_functions.php
@@ -1,11 +1,44 @@
 <?php
 
 // Function which prints "Hello I am Zura"
+/* function hello()
+{
+  echo "Hello I am Achini";
+}
+hello(); */
 
 // Function with argument
+ /*    // 1 ->
+    function hello($name)
+    {
+      echo "Hello I am $name";
+    }
+    hello('Achini');
+    hello('Avishka'); */
 
+    // 2 ->
+/*     function hello($name)
+    {
+      return "Hello I am $name";
+    }
+     echo hello('Achini').'<br>';
+     echo hello('Avishka').'<br>';
+ */
 // Create sum of two functions
+/* function sum ($a, $b)
+{
+  return $a + $b;
+}
+echo sum (4, 10); */
 
 // Create function to sum all numbers using ...$nums
+function sum(...$nums) {
+  $sum = 0;
+  foreach ($nums as $n) {
+    $sum += $n;
+  }
+  return $sum;
+}
+echo sum(1, 2, 3, 4, 5, 6);
 
 // Arrow functions
